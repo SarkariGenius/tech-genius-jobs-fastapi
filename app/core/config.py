8d9dce4968c295556a0b44c79c9a676f9f4aba53
Tech Genius <sarkari.genius@gmail.com>
2026-09-16 19:04:38 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     APP_ENV: str = "development"
 
    
-DATABASE_URL: str = "postgresql+psycopg://postgres.bjecaymebcexkafucldl:YOUR_PASSWORD@aws-0-ap-south-1.pooler.supabase.com:5432/postgres"
+    DATABASE_URL: str = "postgresql+psycopg://postgres.bjecaymebcexkafucldl:YOUR_PASSWORD@aws-0-ap-south-1.pooler.supabase.com:5432/postgres"
 
     JWT_SECRET_KEY: str = "CHANGE_ME"
     JWT_ALGORITHM: str = "HS256"
